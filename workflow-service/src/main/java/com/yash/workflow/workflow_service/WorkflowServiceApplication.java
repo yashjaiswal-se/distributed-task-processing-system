36e6c56a3928b503972cc13aa9ac934aa308e122
Jaisy <yashjaiswal.work@gmail.com>
2026-02-22 16:53:46 +0530

    Initial Spring Boot wroker service  project setup with PostgreSQL configuration

---
+++ workflow-service/src/main/java/com/yash/workflow/workflow_service/WorkflowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yash.workflow.workflow_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowServiceApplication.class, args);
+	}
+
+}
